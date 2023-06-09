7093bf73809a6d21d96c6053621b9d78bcbdee88
George Moschovitis <gmosx@reizu.com>
2023-06-09 20:34:17 +0300

    Refactoring, fix format on save and missing diagnostics

--- src/main.rs
+++ src/main.rs
@@ -1,209 +1,34 @@
-use lsp_server::{Connection, Message, Response};
-use lsp_types::{
-    notification::{DidChangeWatchedFiles, Notification, PublishDiagnostics},
-    request::{Formatting, Request},
-    DidChangeWatchedFilesParams, DocumentFormattingParams, OneOf, Position,
-    PublishDiagnosticsParams, Range, ServerCapabilities, TextEdit, Url,
-};
-use tan::api::parse_string_all;
-use tan_fmt::pretty::Formatter;
-use tan_lint::compute_diagnostics;
-use tracing::{info, trace};
-use tracing_subscriber::util::SubscriberInitExt;
+mod server;
+mod util;
 
-const VERSION: &str = env!("CARGO_PKG_VERSION");
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
-pub fn send_diagnostics(connection: &Connection, uri: Url) -> anyhow::Result<()> {
-    let diagnostics = compute_diagnostics(uri.as_str());
+use crate::server::Server;
 
-    // #Insight
-    // We send a notification even for empty diagnostics to clear previous
-    // diagnostics.
+fn init_tracing() {
+    // #TODO RUST_LOG is not passed from vscode, investigate.
 
-    // if diagnostics.is_empty() {
-    //     return Ok(());
-    // }
+    let fmt_layer = fmt::layer()
+        .with_target(false)
+        .with_writer(std::io::stderr)
+        .with_ansi(false);
 
-    let pdm = PublishDiagnosticsParams {
-        uri: uri.clone(),
-        diagnostics,
-        version: None,
-    };
+    let filter_layer = EnvFilter::try_from_default_env()
+        .or_else(|_| EnvFilter::try_new("info"))
+        .unwrap();
 
-    let notification = lsp_server::Notification {
-        method: PublishDiagnostics::METHOD.to_owned(),
-        params: serde_json::to_value(&pdm).unwrap(),
-    };
-
-    connection
-        .sender
-        .send(Message::Notification(notification))?;
-
-    Ok(())
-}
-
-fn run(connection: Connection, _params: serde_json::Value) -> anyhow::Result<()> {
-    // #TODO use params to get root_uri and perform initial diagnostics for all files.
-    // let params: InitializeParams = serde_json::from_value(params).unwrap();
-    // eprintln!("{params:#?}");
-
-    for msg in &connection.receiver {
-        trace!("Got msg: {:?}.", msg);
-        match msg {
-            Message::Request(req) => {
-                if connection.handle_shutdown(&req)? {
-                    return Ok(());
-                }
-                trace!("got request: {:?}", req);
-                // match cast::<GotoDefinition>(req.clone()) {
-                //     Ok((id, params)) => {
-                //         eprintln!("got gotoDefinition request #{id}: {params:?}");
-                //         let result = Some(GotoDefinitionResponse::Array(Vec::new()));
-                //         let result = serde_json::to_value(&result).unwrap();
-                //         let resp = Response {
-                //             id,
-                //             result: Some(result),
-                //             error: None,
-                //         };
-                //         connection.sender.send(Message::Response(resp))?;
-                //         continue;
-                //     }
-                //     Err(err @ ExtractError::JsonError { .. }) => panic!("{err:?}"),
-                //     Err(ExtractError::MethodMismatch(req)) => req,
-                // };
-                // match cast::<References>(req.clone()) {
-                //     Ok((id, params)) => {
-                //         eprintln!("got references request #{id}: {params:?}");
-                //         let result = Some(Vec::<String>::new());
-                //         let result = serde_json::to_value(&result).unwrap();
-                //         let resp = Response {
-                //             id,
-                //             result: Some(result),
-                //             error: None,
-                //         };
-                //         connection.sender.send(Message::Response(resp))?;
-                //         continue;
-                //     }
-                //     Err(err @ ExtractError::JsonError { .. }) => panic!("{err:?}"),
-                //     Err(ExtractError::MethodMismatch(req)) => req,
-                // };
-
-                match req.method.as_ref() {
-                    Formatting::METHOD => {
-                        let (id, params) =
-                            req.extract::<DocumentFormattingParams>(Formatting::METHOD)?;
-
-                        let path = params.text_document.uri.path();
-                        let input = std::fs::read_to_string(path)?;
-
-                        let Ok(exprs) = parse_string_all(&input) else {
-                            return Err(anyhow::anyhow!("Error"));
-                        };
-
-                        let mut formatter = Formatter::new(&exprs);
-                        let formatted = formatter.format();
-
-                        // Select the whole document dore replacement
-                        let start = Position::new(0, 0);
-                        let end = Position::new(u32::MAX, u32::MAX);
-                        let document_range = Range::new(start, end);
-
-                        let result = Some(vec![TextEdit::new(document_range, formatted)]);
-                        let result = serde_json::to_value(&result).unwrap();
-                        let resp = Response {
-                            id,
-                            result: Some(result),
-                            error: None,
-                        };
-
-                        connection.sender.send(Message::Response(resp))?;
-
-                        continue;
-                    }
-                    _ => continue,
-                }
-            }
-            Message::Response(resp) => {
-                trace!("Got response: {:?}.", resp);
-            }
-            Message::Notification(event) => {
-                trace!("got notification: {:?}.", event);
-                if let Ok(event) =
-                    event.extract::<DidChangeWatchedFilesParams>(DidChangeWatchedFiles::METHOD)
-                {
-                    for change in event.changes {
-                        // let path = change.uri.path();
-                        // let input = std::fs::read_to_string(path)?;
-                        // let result = parse_string_all(&input);
-
-                        // let diagnostics = match result {
-                        //     Ok(exprs) => {
-                        //         let mut diagnostics = Vec::new();
-
-                        //         let mut lint = SnakeCaseNamesLint::new(&input);
-                        //         lint.run(&exprs);
-                        //         diagnostics.append(&mut lint.diagnostics);
-
-                        //         diagnostics
-                        //     }
-                        //     Err(errors) => gen_parse_error_diagnostics(&input, errors)?,
-                        // };
-
-                        // let pdm = PublishDiagnosticsParams {
-                        //     uri: change.uri.clone(),
-                        //     diagnostics,
-                        //     version: None,
-                        // };
-
-                        // let notification = lsp_server::Notification {
-                        //     method: PublishDiagnostics::METHOD.to_owned(),
-                        //     params: serde_json::to_value(&pdm).unwrap(),
-                        // };
-
-                        // connection
-                        //     .sender
-                        //     .send(Message::Notification(notification))?;
-
-                        send_diagnostics(&connection, change.uri)?;
-                    }
-                }
-            }
-        }
-    }
-    Ok(())
+    tracing_subscriber::registry()
+        .with(filter_layer)
+        .with(fmt_layer)
+        .init();
 }
 
 fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt()
-        .with_writer(std::io::stderr)
-        .with_ansi(false)
-        .finish()
-        .init();
+    init_tracing();
 
-    info!("Starting LSP server, v{VERSION}...");
-
-    // Create the connection using stdio as the transport kind.
-    let (connection, io_threads) = Connection::stdio();
-
-    let server_capabilities = serde_json::to_value(&ServerCapabilities {
-        // definition_provider: Some(OneOf::Left(true)),
-        // references_provider: Some(OneOf::Left(true)),
-        document_formatting_provider: Some(OneOf::Left(true)),
-        ..Default::default()
-    })
-    .unwrap();
-
-    let initialization_params = connection.initialize(server_capabilities)?;
-
-    info!("Started.");
-
-    // Run the server.
-    run(connection, initialization_params)?;
-
-    // Wait for the two threads to end (typically by trigger LSP Exit event).
-    io_threads.join()?;
-
-    info!("Shutting down server...");
+    // #TODO mut sucks here.
+    let mut server = Server::new();
+    server.run()?;
 
     Ok(())
 }
---
+++ src/server.rs
@@ -0,0 +1,228 @@
+use std::collections::HashMap;
+
+use anyhow::anyhow;
+use lsp_server::{Connection, Message, Response};
+use lsp_types::{
+    notification::{DidChangeTextDocument, DidOpenTextDocument, Notification, PublishDiagnostics},
+    request::{Formatting, Request},
+    DidChangeTextDocumentParams, DidOpenTextDocumentParams, DocumentFormattingParams, OneOf,
+    Position, PublishDiagnosticsParams, Range, ServerCapabilities, TextDocumentSyncKind, TextEdit,
+    Url,
+};
+use tan::api::parse_string_all;
+use tan_fmt::pretty::Formatter;
+use tan_lint::compute_diagnostics;
+use tracing::{info, trace};
+
+use crate::util::VERSION;
+
+pub struct Server {
+    documents: HashMap<String, String>,
+}
+
+// #TODO split further into methods.
+
+impl Server {
+    pub fn new() -> Self {
+        Self {
+            documents: HashMap::default(),
+        }
+    }
+
+    pub fn run(&mut self) -> anyhow::Result<()> {
+        info!("Starting LSP server, v{}...", VERSION);
+
+        let (connection, io_threads) = Connection::stdio();
+
+        let server_capabilities = serde_json::to_value(&ServerCapabilities {
+            // definition_provider: Some(OneOf::Left(true)),
+            // references_provider: Some(OneOf::Left(true)),
+            // #Insight Enables didOpen/didChange notifications.
+            text_document_sync: Some(lsp_types::TextDocumentSyncCapability::Kind(
+                TextDocumentSyncKind::FULL,
+            )),
+            rename_provider: Some(OneOf::Left(true)),
+            document_formatting_provider: Some(OneOf::Left(true)),
+            ..Default::default()
+        })
+        .unwrap();
+
+        let initialization_params = connection.initialize(server_capabilities)?;
+
+        info!("Started.");
+
+        // Run the server.
+        self.run_loop(connection, initialization_params)?;
+
+        // Wait for the two threads to end (typically by trigger LSP Exit event).
+        io_threads.join()?;
+
+        info!("Shutting down server...");
+
+        Ok(())
+    }
+
+    // #TODO return a more precise result.
+    pub fn send_diagnostics(&self, connection: &Connection, uri: Url) -> anyhow::Result<()> {
+        let Some(input) = self.documents.get(uri.as_str()) else {
+            return Err(anyhow!("Unknown document").context("in send_diagnostics"));
+        };
+
+        let diagnostics = compute_diagnostics(input);
+
+        let pdm = PublishDiagnosticsParams {
+            uri: uri.clone(),
+            diagnostics,
+            version: None,
+        };
+
+        let notification = lsp_server::Notification {
+            method: PublishDiagnostics::METHOD.to_owned(),
+            params: serde_json::to_value(&pdm).unwrap(),
+        };
+
+        connection
+            .sender
+            .send(Message::Notification(notification))?;
+
+        Ok(())
+    }
+
+    pub fn run_loop(
+        &mut self,
+        connection: Connection,
+        _params: serde_json::Value,
+    ) -> anyhow::Result<()> {
+        // #TODO use params to get root_uri and perform initial diagnostics for all files.
+        // let params: InitializeParams = serde_json::from_value(params).unwrap();
+        // eprintln!("{params:#?}");
+
+        for msg in &connection.receiver {
+            trace!("Got msg: {:?}.", msg);
+            match msg {
+                Message::Request(req) => {
+                    if connection.handle_shutdown(&req)? {
+                        return Ok(());
+                    }
+                    trace!("got request: {:?}", req);
+                    // match cast::<GotoDefinition>(req.clone()) {
+                    //     Ok((id, params)) => {
+                    //         eprintln!("got gotoDefinition request #{id}: {params:?}");
+                    //         let result = Some(GotoDefinitionResponse::Array(Vec::new()));
+                    //         let result = serde_json::to_value(&result).unwrap();
+                    //         let resp = Response {
+                    //             id,
+                    //             result: Some(result),
+                    //             error: None,
+                    //         };
+                    //         connection.sender.send(Message::Response(resp))?;
+                    //         continue;
+                    //     }
+                    //     Err(err @ ExtractError::JsonError { .. }) => panic!("{err:?}"),
+                    //     Err(ExtractError::MethodMismatch(req)) => req,
+                    // };
+                    // match cast::<References>(req.clone()) {
+                    //     Ok((id, params)) => {
+                    //         eprintln!("got references request #{id}: {params:?}");
+                    //         let result = Some(Vec::<String>::new());
+                    //         let result = serde_json::to_value(&result).unwrap();
+                    //         let resp = Response {
+                    //             id,
+                    //             result: Some(result),
+                    //             error: None,
+                    //         };
+                    //         connection.sender.send(Message::Response(resp))?;
+                    //         continue;
+                    //     }
+                    //     Err(err @ ExtractError::JsonError { .. }) => panic!("{err:?}"),
+                    //     Err(ExtractError::MethodMismatch(req)) => req,
+                    // };
+
+                    match req.method.as_ref() {
+                        Formatting::METHOD => {
+                            let (id, params) =
+                                req.extract::<DocumentFormattingParams>(Formatting::METHOD)?;
+
+                            let document = params.text_document;
+
+                            let Some(input) = self.documents.get(document.uri.as_str()) else {
+                                return Err(anyhow!("Unknown document").context("in Formatting::METHOD"));
+                            };
+
+                            let Ok(exprs) = parse_string_all(&input) else {
+                                return Err(anyhow::anyhow!("Error"));
+                            };
+
+                            let mut formatter = Formatter::new(&exprs);
+                            let formatted = formatter.format();
+
+                            // Select the whole document dore replacement
+                            let start = Position::new(0, 0);
+                            let end = Position::new(u32::MAX, u32::MAX);
+                            let document_range = Range::new(start, end);
+
+                            let result = Some(vec![TextEdit::new(document_range, formatted)]);
+                            let result = serde_json::to_value(&result).unwrap();
+                            let resp = Response {
+                                id,
+                                result: Some(result),
+                                error: None,
+                            };
+
+                            connection.sender.send(Message::Response(resp))?;
+
+                            continue;
+                        }
+                        _ => continue,
+                    }
+                }
+                Message::Response(resp) => {
+                    trace!("Got response: {:?}.", resp);
+                }
+                Message::Notification(notification) => {
+                    info!("got notification: {:?}.", notification);
+
+                    match notification.method.as_ref() {
+                        "textDocument/didOpen" => {
+                            if let Ok(params) = notification
+                                .extract::<DidOpenTextDocumentParams>(DidOpenTextDocument::METHOD)
+                            {
+                                let document = params.text_document;
+                                self.documents
+                                    .insert(document.uri.to_string(), document.text);
+                                self.send_diagnostics(&connection, document.uri)?;
+                            }
+                        }
+                        "textDocument/didChange" => {
+                            if let Ok(params) = notification.extract::<DidChangeTextDocumentParams>(
+                                DidChangeTextDocument::METHOD,
+                            ) {
+                                let document = params.text_document;
+                                let changes = params.content_changes;
+                                if let Some(change) = changes.first() {
+                                    self.documents
+                                        .insert(document.uri.to_string(), change.text.clone());
+                                    self.send_diagnostics(&connection, document.uri)?;
+                                }
+                            }
+                        }
+                        _ => {
+                            eprintln!("Unhandled: {}", notification.method);
+                        }
+                    }
+
+                    // if let Ok(event) =
+                    //     &event.extract::<DidChangeTextDocumentParams>(DidChangeTextDocument::METHOD)
+                    // {
+                    //     for change in event.content_changes.into_iter() {
+                    //         dbg!(change.text);
+                    //     }
+                    // }
+
+                    // #TODO try to switch to incremental sync.
+                }
+            }
+        }
+        Ok(())
+    }
+}
